4de38baa8e76f34a6b70d1b463086bb28975abd7
ItzIcyGaming <73133185+TrulyItzIcyGaming@users.noreply.github.com>
2025-03-22 01:30:59 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('header');
-    const container = document.querySelector('.container');
-    const headerHeight = header.offsetHeight;
-    document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
-});
